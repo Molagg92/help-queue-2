aa22c619c898065f320578e7e804d41e4ec60d5b
Erik Z <molagg92@gmail.com>
2023-10-01 08:03:22 -0700

    fixxed a bug, index wasnt showing

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -7,11 +7,13 @@ describe('Help Queue actions', () => {
       id: 1
     });
   });
+
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
       type: 'TOGGLE_FORM'
     });
   });
+
   it('addTicket should create ADD_TICKET action', () => {
     expect(actions.addTicket({
       names: 'Jo and Jasmine', 
@@ -26,4 +28,5 @@ describe('Help Queue actions', () => {
       id: 1
     });
   });
+
 });
